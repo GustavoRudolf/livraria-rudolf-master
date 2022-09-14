2a7d3a9f21d928fab2b705f73da61bae60faa3a6
Astatsu <vinijao123690@gmail.com>
2022-09-14 15:17:22 -0300

    salvo

---
+++ src/api/editoras.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+export default class EditorasApi {
+  async buscarTodasAsEditoras() {
+    const response = await axios.get("https://livraria-gustavorudolf.herokuapp.com/editoras");
+    return response.data;
+  }
+  async buscarEditora(id) {
+    const response = await axios.get(`https://livraria-gustavorudolf.herokuapp.com/editoras/${id}`);
+    return response.data;
+  }
+
+  async adicionarEditora(editora) {
+    const response = await axios.post(
+      "https://livraria-gustavorudolf.herokuapp.com/editoras",
+      editora
+    );
+    return response.data;
+  }
+
+  async excluirEditora(id) {
+    const response = await axios.delete(`https://livraria-gustavorudolf.herokuapp.com/editoras/${id}`);
+    return response.data;
+  }
+
+  async atualizarEditora(editora) {
+    const response = await axios.put(
+      `https://livraria-gustavorudolf.herokuapp.com/editoras/${editora.id}`,
+      editora
+    );
+    return response.data;
+  }
+}
